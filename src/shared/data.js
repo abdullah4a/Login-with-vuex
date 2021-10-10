3c7e8d2d32c7590689bf654e56842e9812b98092
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-10-10 15:04:07 +0500

    Installation

---
+++ src/shared/data.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import { API } from './config'
+const Getheroes = async function() {
+    try {
+        const responce = await axios.get(`${API}/apidata.json`);
+        const data = parseErrors(responce, 200)
+        return data
+    } catch (err) {
+        console.error("Data fetch Error" + err);
+    }
+}
+
+const Addhero = async function(U) {
+    try {
+        const responce = await axios.post(`${API}/apidata.json`, U);
+        const data = parseErrors(responce, 201)
+        return data
+    } catch (err) {
+        console.error("Data fetch Error" + err);
+    }
+}
+const parseErrors = function(responce, code) {
+    if (responce.status != code) throw Error("status Failure");
+    if (!responce.data) return ""
+    const data = responce.data
+    return data
+}
+export const Data = {
+    Getheroes,
+    Addhero
+}
---
+++ src/store/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+const state = {
+    // a: 2,
+    UserLogin: {
+        UID: '2aaaabbbbcccd1',
+        email: "abullah@someemail.com",
+        Pwd: "abc"
+    },
+    Islogin: false,
+    error: "Error in login\n Check again your Email and Password"
+};
+const mutations = {
+    UserLoginmut(state, user) {
+        if (state.UserLogin.email === user.email && state.UserLogin.Pwd) {
+            user.UID = state.UserLogin.UID;
+            user.Islogin = true
+        } else {
+            user.Islogin = state.Islogin
+            user.error = state.error
+        }
+    }
+
+}
+const actions = {
+    LoginUserAct({ commit }, user) {
+        commit("UserLoginmut", user)
+    }
+}
+const modules = {}
+
+export default new Vuex.Store({
+    state,
+    mutations,
+    actions,
+    modules
+})
