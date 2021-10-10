cac31fc41286a8f88ee833dfa2ecc15758cce58a
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-10-10 15:27:13 +0500

    Getting Admins and User Separately

--- src/shared/data.js
+++ src/shared/data.js
@@ -1,24 +1,32 @@
 import axios from 'axios'
 import { API } from './config'
-const Getheroes = async function() {
+const GetUsers = async function() {
     try {
-        const responce = await axios.get(`${API}/apidata.json`);
+        const responce = await axios.get(`${API}/apidata.json/User`);
         const data = parseErrors(responce, 200)
         return data
     } catch (err) {
         console.error("Data fetch Error" + err);
     }
 }
-
-const Addhero = async function(U) {
-    try {
-        const responce = await axios.post(`${API}/apidata.json`, U);
-        const data = parseErrors(responce, 201)
-        return data
-    } catch (err) {
-        console.error("Data fetch Error" + err);
+const GetAdmins = async function() {
+        try {
+            const responce = await axios.get(`${API}/apidata.json/Admin`);
+            const data = parseErrors(responce, 200)
+            return data
+        } catch (err) {
+            console.error("Data fetch Error" + err);
+        }
     }
-}
+    // const Addhero = async function(U) {
+    //     try {
+    //         const responce = await axios.post(`${API}/apidata.json`, U);
+    //         const data = parseErrors(responce, 201)
+    //         return data
+    //     } catch (err) {
+    //         console.error("Data fetch Error" + err);
+    //     }
+    // }
 const parseErrors = function(responce, code) {
     if (responce.status != code) throw Error("status Failure");
     if (!responce.data) return ""
@@ -26,6 +34,6 @@ const parseErrors = function(responce, code) {
     return data
 }
 export const Data = {
-    Getheroes,
-    Addhero
+    GetAdmins,
+    GetUsers
 }
