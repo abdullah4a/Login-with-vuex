3532b06fba7abc1a264c8f33ea2310854bbbc6b7
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-10-11 16:48:33 +0500

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,21 +1,46 @@
 <template>
   <v-app>
-    <v-container>
-      <v-card>
-        <v-card-title>
-          <v-icon>mdi-login</v-icon>
-          &nbsp;
-          <span class="text-h4 text-center">login</span>
-        </v-card-title>
-        <v-card-actions>
-          <v-btn plain @click="GotoApp">
-            Login
-          </v-btn>
-          <v-btn>
-            Register
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+    <v-container text-center>
+      <v-row row wrap>
+        <v-col>
+          <v-card width="550">
+            <v-card-title class="justify-center">
+              <v-icon>mdi-login</v-icon>
+              &nbsp;
+              <span class="text-h4">login</span>
+            </v-card-title>
+            <v-card-text>
+              <v-form ref="form" v-model="valid" lazy-validation>
+                <v-text-field
+                  v-model="email"
+                  :rules="emailRules"
+                  label="E-mail"
+                  required
+                  outlined
+                ></v-text-field>
+                <v-text-field
+                  v-model="password"
+                  :counter="10"
+                  :rules="PasswordRules"
+                  type="password"
+                  label="Password"
+                  required
+                  outlined
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions class="justify-center">
+              <v-btn plain :disabled="!valid" @click="GotoApp">
+                <v-icon>mdi-login</v-icon>&nbsp;
+                <span>Login</span>
+              </v-btn>
+              <v-btn plain>
+                <v-icon> mdi-cancel </v-icon> &nbsp; <span>Cancel</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-container>
   </v-app>
 </template>
@@ -25,11 +50,25 @@ export default {
     data() {
       return {
         User: {
+          valid: true,
+          PasswordRules: [
+            (v) => !!v || "Password is required",
+            (v) =>
+              (v && v.length >= 8) ||
+              "Password must be greater than 8 chararacters",
+          ],
+          password: "",
+          email: "",
+          emailRules: [
+            (v) => !!v || "E-mail is required",
+            (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+          ],
           Islogin: true,
         },
       };
     },
     GotoApp() {
+      this.$refs.form.validate();
     },
   },
 };
